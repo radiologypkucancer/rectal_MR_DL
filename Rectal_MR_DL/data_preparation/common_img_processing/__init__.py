bbebba5c172c2e5d97f5383e332e5ad99fc574d6
radiologypkucancer <52984778+radiologypkucancer@users.noreply.github.com>
2021-12-18 17:54:58 +0800

    Delete Rectal_MR_DL directory

--- Rectal_MR_DL/data_preparation/common_img_processing/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-    Program: BJCH DL lab
-    file name: __init__.py
-
-"""
-from . import augmentation, cropping, labels, resampling
--- Rectal_MR_DL/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-    Program: BJCH DL lab
-    file name: __init__.py
-
-"""
